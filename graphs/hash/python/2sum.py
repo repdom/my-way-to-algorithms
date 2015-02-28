ce1d966364e1ecf48c3ff1d75a9e3ae0dd3cdd28
xala3pa <salazar3pa@gmail.com>
2015-02-28 11:46:09 +0100

    2-sum algo implemented using a dictionary

--- graphs/hash/python/2sum.py
+++ graphs/hash/python/2sum.py
@@ -1,9 +1,27 @@
 import sys
 import threading
 
+def TwoSum(lst, target):
+    hashtable = dict()
+
+    for num in lst:
+        hashtable[num] = True
+
+    for x in hashtable:
+        y =  target - x
+        if y in  hashtable and x != y:
+            return (x, y)
+    return None
+
+
 def main():
     lines = open('algo1-programming_prob-2sum.txt').read().splitlines()
     data = map(lambda x: int(x), lines)
+    count = 0
+    for target in range(-10000,10000+1):
+        if TwoSum(data, target):
+            count += 1
+    print count
 
 if __name__ == '__main__':
     threading.stack_size(67108864) # 64MB stack
